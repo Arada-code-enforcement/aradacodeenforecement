fc420098391c976e68156210b26cce79bdc8b9ce
Arada-code-enforcement <alemusamii99@gmail.com>
2026-05-14 11:54:27 +0300

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -65,6 +65,9 @@ const Footer = () => {
                 <div>
                   <span className="block text-white font-medium">Email</span>
                   <a href="mailto:info@arada-enforcement.com" className="hover:text-primary transition-colors">info@arada-enforcement.com</a>
+                </div> 
+                <span className="block text-white font-medium">Email</span>
+                  <a href="mailto:alexsamii554@gmail.com" className="hover:text-primary transition-colors">alexsamii554@gmail.com</a>
                 </div>
               </li>
             </ul>
